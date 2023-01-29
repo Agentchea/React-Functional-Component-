b337f60603df3787756e8c41f8003d93c61dee96
Derrick Chea <derrick.c.chea@gmail.com>
2023-01-29 16:39:49 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import greet from './components/greet';
+
+function App() {
+  return (
+    <div className="App">
+  <greet/>
+    </div>
+  );
+}
+
+export default App;
